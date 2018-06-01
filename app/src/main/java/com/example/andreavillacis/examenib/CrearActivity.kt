1b575ec510d2e1de5596450301857f535e288d68
Avill2 <andrea.villacis@epn.edu.ec>
2018-06-01 11:44:36 -0500

    creacion de recylcer view listar activity agregar recursos.

--- app/src/main/java/com/example/andreavillacis/examenib/CrearActivity.kt
+++ app/src/main/java/com/example/andreavillacis/examenib/CrearActivity.kt
@@ -2,6 +2,7 @@ package com.example.andreavillacis.examenib
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_crear.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,12 +16,11 @@ class CrearActivity : AppCompatActivity() {
     }
 
     fun Crear_conductor_auto(){
-        val nombres = text_nombre_edit.text.toString()
-        val apellidos = text_apellido_edit.text.toString()
-        val fechanacimiento = text_fechaNaci_edit.text.toString()
-        val numeroautos = text_nombre_edit.text.toString()
-        val licencia = text_nombre_edit.text.toString()
+        val nombres = editText_nombres.text.toString()
+        val apellidos = editText_apellidos.text.toString()
+        val fechanacimiento = editText_fechanac.text.toString()
+        val numeroautos = editText_numeroautos.text.toString()
+        val licencia = editText_licencia.text.toString()
     }
-
 
 }
