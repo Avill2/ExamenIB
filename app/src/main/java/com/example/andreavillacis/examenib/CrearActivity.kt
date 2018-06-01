5e0c60d7b159b2f33c28bf67da96c7ed6e374e0a
Avill2 <andrea.villacis@epn.edu.ec>
2018-06-01 10:21:32 -0500

    creacion de el boton crear aumentar mas recursos en ingles

---
+++ app/src/main/java/com/example/andreavillacis/examenib/CrearActivity.kt
@@ -0,0 +1,12 @@
+package com.example.andreavillacis.examenib
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear)
+    }
+}
