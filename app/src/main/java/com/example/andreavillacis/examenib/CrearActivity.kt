3e4ce6b7710cbb912c193220173ba556d75ac8a6
Avill2 <andrea.villacis@epn.edu.ec>
2018-06-02 17:48:15 -0500

    creacion de conductor y recycler view
    utilizar mas recursos

--- app/src/main/java/com/example/andreavillacis/examenib/CrearActivity.kt
+++ app/src/main/java/com/example/andreavillacis/examenib/CrearActivity.kt
@@ -1,10 +1,12 @@
 package com.example.andreavillacis.examenib
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_crear.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class CrearActivity : AppCompatActivity() {
 
@@ -15,12 +17,37 @@ class CrearActivity : AppCompatActivity() {
         button_crear.setOnClickListener{view: View -> Crear_conductor_auto()}
     }
 
+    class Conductor_persona (var nombre:String,
+                     var apellido: String,
+                     var fecha_nacimiento: Date,
+                     var numeroAutos:Int,
+                     var licenciaValida:Boolean){
+    }
+
     fun Crear_conductor_auto(){
         val nombres = editText_nombres.text.toString()
         val apellidos = editText_apellidos.text.toString()
         val fechanacimiento = editText_fechanac.text.toString()
         val numeroautos = editText_numeroautos.text.toString()
         val licencia = editText_licencia.text.toString()
+        irActividadListar()
+    }
+    fun irActividadListar(){
+        val intent = Intent(this, ListarActivity::class.java)
+        startActivity(intent)
+    }
+
+    class companion{
+        companion object {
+            var conductor: ArrayList<Conductor_persona> = ArrayList()
+
+            init {
+                conductor.add(Conductor_persona("Algo1" , "algo2", Date() , 3, true ))
+                conductor.add(Conductor_persona("Algo3" , "algo4", Date() , 1, false ))
+                conductor.add(Conductor_persona("Algo5" , "algo6", Date() , 5, true ))
+            }
+
+        }
     }
 
 }
--- app/src/main/java/com/example/andreavillacis/examenib/ListarActivity.kt
+++ app/src/main/java/com/example/andreavillacis/examenib/ListarActivity.kt
@@ -2,6 +2,11 @@ package com.example.andreavillacis.examenib
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import java.util.*
 
 class ListarActivity : AppCompatActivity() {
 
@@ -9,4 +14,30 @@ class ListarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listar)
     }
+}
+
+
+
+class ConductorAdaptador(private val listaConductor :List<CrearActivity.Conductor_persona>): RecyclerView.Adapter<ConductorAdaptador.MyViewHolder>(){
+    inner  class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+        lateinit var nombre: TextView
+        lateinit var apellido: TextView
+        var
+
+    }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
 }
